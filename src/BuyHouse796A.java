99ea13051208ecc04bb9208060e28da7732b9d34
willwsxu <willwsxu@gmail.com>
2017-04-24 09:15:57 -0500

    contest 796

---
+++ src/BuyHouse796A.java
@@ -0,0 +1,27 @@
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+
+public class BuyHouse796A {
+    
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {        
+        int N = scan.nextInt();  // 2 ≤ n ≤ 100, 1 ≤ m ≤ n, 1 ≤ k ≤ 100
+        int m = scan.nextInt();  // girl's hourse
+        int k = scan.nextInt();  // money
+        int A[]=new int[N]; // house price, 0 ≤ ai ≤ 100
+        int d=999999; //
+        m--;  // index from 0
+        for (int i=0; i<N; i++) {
+            A[i]=scan.nextInt();
+            if (A[i]>0 && A[i]<=k) {
+                int d1 = m>i?(m-i):(i-m);
+                if (d1<d)
+                    d=d1;
+            }
+        }
+        out.println(d*10);
+    }
+}
---
+++ src/FindBone796B.java
@@ -0,0 +1,56 @@
+
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class FindBone796B {
+    
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {        
+        int N = scan.nextInt();  // 2 ≤ n ≤ 10^6, 1 ≤ m ≤ n, 1 ≤ k ≤ 3·10^5
+        int m = scan.nextInt();  // hole
+        int k = scan.nextInt();  // swaps
+        Set<Integer> holes = new HashSet<>();
+        for (int i=0; i<m; i++)
+            holes.add(scan.nextInt());
+        if ( holes.contains(1) ) {
+            out.println(1);
+            return;
+        }
+        int last=1;
+        for (int i=0; i<k; i++) {
+            String line = scan.nextLine();
+            if (line.isEmpty())
+                line = scan.nextLine();
+            String[] swap = line.split(" ");
+            int c1 = Integer.parseInt(swap[0]);
+            int c2 = Integer.parseInt(swap[1]);
+            int other=0;
+            if (c1==last)  {
+                other = c2;
+            } else if (c2==last)  {
+                other = c1;
+            }
+            else // no bones
+                continue;
+            last = other;
+            if (holes.contains(other)) {
+                out.println(other);
+                break;
+            }
+            if (i==k-1) 
+                out.println(other);
+        }
+    }
+}
+/*
+5 1 5
+2
+2 3
+3 4
+2 1
+4 5
+*/
