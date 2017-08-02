25bed9314d03d8d64b1b340ec50cc0f304128c1e
willwsxu <willwsxu@gmail.com>
2017-08-02 15:25:53 -0500

    replace TreeSet with PriorityQueue to sort outgoing edges

--- src/MinLabel.java
+++ src/MinLabel.java
@@ -1,13 +1,17 @@
 
 import static java.lang.System.out;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeSet;
 
+// TreeSet can not store item when IntPair.second is not unique, even overrides
+// HashSet can store IntPair even if second is duplicate
+// For this reason, PriorityQUeue is chosen to sort outgoing edges
 public class MinLabel {
     DAG dag, dagr;
     MinLabel()
@@ -22,36 +26,45 @@ public class MinLabel {
             dag.addEdge(u-1, v-1);
             dagr.addEdge(v-1, u-1);
         }
+        //dag.print();
+        //dagr.print();
     }
     
     void solve()
     {
         // sort vertex by outgoing edges
-        TreeSet<Map.Entry<Integer,Integer>> sorted = new TreeSet<>((e1,e2)->e1.getValue()-e2.getValue());
+        PriorityQueue<IntPair> sorted = new PriorityQueue<>((e1,e2)->e1.int2()-e2.int2());
         for (int i=0; i<dag.V(); i++) {
             List<Integer> a = dag.adj(i);
-            sorted.add(new AbstractMap.SimpleEntry(i+1, a.size()));
+            IntPair e=new IntPair(i+1, a.size());
+            sorted.add(e);
         }
+        //out.println(sorted);
         int ans[]=new int[dag.V()];
+        Arrays.fill(ans, -1);
         int label=dag.V();
         while (!sorted.isEmpty()) {
-            Map.Entry<Integer,Integer> e = sorted.pollFirst();
-            ans[e.getKey()-1]=label--;
+            IntPair e = sorted.poll();
+            if ( ans[e.int1()-1]>=0)  // obsolete items
+                continue;
+            ans[e.int1()-1]=label--;
             
             // remove any edge connected to this vertex
-            List<Integer> r = dagr.adj(e.getKey()-1);
+            List<Integer> r = dagr.adj(e.int1()-1);
             for (int j=0; j<r.size(); j++) {
                 List<Integer> uL=dag.adj(r.get(j)); // find u conneced to v
-                Map.Entry<Integer,Integer> out = new AbstractMap.SimpleEntry(r.get(j)+1, uL.size());
-                sorted.remove(out);
-                out.setValue(out.getValue()-1);
-                sorted.add(out);
-                uL.remove(new Integer(e.getKey()-1));
+                IntPair out = new IntPair(r.get(j)+1, uL.size()-1);
+                sorted.add(out);  // let old items become obsolete, don't call remove
+                uL.remove(new Integer(e.int1()-1));
             }
         }
-        out.println(Arrays.toString(ans));
+        for (int b: ans) {
+            out.print(b);
+            out.print(" ");
+        }
+        out.println();
     }
-    
+        
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {        
@@ -84,5 +97,50 @@ class DAG { // unweighted, DAG
     public List<Integer> adj(int u)
     {
         return adj.get(u);
+    }
+    void print()
+    {
+        out.println(adj);
+    }
+}
+
+
+class IntPair  // pair of int
+{
+    int first;
+    int second;
+    public IntPair(int f, int s)
+    {
+        first=f;
+        second=s;
+    }
+    public int int1()
+    {
+        return first;
+    }
+    public int int2()
+    {
+        return second;
+    }
+    @Override
+    public boolean equals(Object s)
+    {
+        out.println(this.toString()+" equal "+s.toString());
+        if (s instanceof IntPair) {
+            IntPair other =(IntPair)s;
+            return first==other.first && second==other.second;
+        }
+        return false;
+    }
+    @Override
+    public int hashCode()
+    {
+        out.println("hashCode");
+        return (int)(first*second);
+    }
+    @Override
+    public String toString()
+    {
+        return first+":"+second;
     }
 }
